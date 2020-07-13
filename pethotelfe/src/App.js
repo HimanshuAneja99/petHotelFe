801a9624a0f7de010ff816fca764f32156c4aa7c
harsh2000yadav <harsh2000yadav@gmail.com>
2020-07-13 18:04:13 +0530

    footer

--- pethotelfe/src/App.js
+++ pethotelfe/src/App.js
@@ -15,7 +15,8 @@ function App() {
       <div style={{marginBottom:'500px', marginTop: '100px',
         marginLeft: '100px' }}>
         <h1>This is the heading of the page</h1>
-        <p>This is the content of the page.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
       </div>
       <FooterPage />
