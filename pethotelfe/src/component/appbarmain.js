ac889fe0f49f52bfdb9bdd1adbd7ec5041567dbe
harsh2000yadav <harsh2000yadav@gmail.com>
2020-07-20 18:23:02 +0530

    Overlay Login

--- pethotelfe/src/component/appbarmain.js
+++ pethotelfe/src/component/appbarmain.js
@@ -19,6 +19,15 @@ import Link from "react-router-dom/Link";
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -225,7 +234,7 @@ export default function Navbar() {
             /> 
             </div>*/}
             <div className={classes.navlink}>
-                <Button color="inherit" type="button" onClick={handleOpen}>Booking</Button>
+                <Button color="inherit">Booking</Button>
             </div>
             <div className={classes.navlink}>
                 <Button color="inherit">About Us</Button>
@@ -252,8 +261,67 @@ export default function Navbar() {
       >
         <Fade in={open}>
           <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
+          <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+     
+    </Container>
           </div>
         </Fade>
       </Modal>
@@ -272,7 +340,7 @@ export default function Navbar() {
               </Badge>
             </IconButton> */}
             <div className={classes.authlink}>
-                <Button variant="outlined" color="inherit" component={Link} to="/login">
+                <Button variant="outlined" color="inherit" type="button" onClick={handleOpen}>
                      Login
                 </Button>
             </div>
