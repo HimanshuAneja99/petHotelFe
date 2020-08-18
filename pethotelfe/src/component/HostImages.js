dd4331e602445459380ec2ae77ab5e6241fd4236
harsh2000yadav <harsh2000yadav@gmail.com>
2020-08-18 14:31:35 +0530

    Host Profile

--- pethotelfe/src/component/HostImages.js
+++ pethotelfe/src/component/HostImages.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import Grid from '@material-ui/core/Grid';
 
+
 export default class HostImages extends Component{
     render(){
         return(
--- pethotelfe/src/pages/HostInfo.js
+++ pethotelfe/src/pages/HostInfo.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react'
 import HostImages from '../component/HostImages'
+import Grid from '@material-ui/core/Grid';
+import HostAbout from '../component/HostAbout';
+import HostPrice from '../component/HostPrice';
 
 export default class HostInfo extends Component{
     render(){
         return(
             <div>
-                <h1>Host info</h1>
-                <HostImages/>
+                <Grid container spacing={5}>
+                    <Grid item xs={12}>
+                        <HostImages/>
+                    </Grid>
+                    <Grid item xs={7}>
+                        <HostAbout/>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <HostPrice/>
+                    </Grid>
+                </Grid>
+                
+                
             </div>
         )
     }
